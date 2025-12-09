a07e8dd5c1393b643116089ae94a2c0abd003818
Jan Hansen Tiu <tiujanhansen@gmail.com>
2025-12-09 17:45:13 +0800

    Forgot to actually add the wallet in the previous commit lol.

---
+++ app/Models/Wallet.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Wallet extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'public_key',
+        'private_key',
+        'wallet',
+    ];
+
+    protected $casts = [
+        'private_key' => 'encrypted',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
